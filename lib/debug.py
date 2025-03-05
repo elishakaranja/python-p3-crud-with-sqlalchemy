d6d3280f41149eaca9e404f72444315eaaa2b936
Elisha Karanja <elishadominic74@gmail.com>
2025-03-05 22:08:39 +0300

    practiced crud

--- lib/debug.py
+++ lib/debug.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,func
+from datetime import datetime
+import ipdb
 
 from sqlalchemy_sandbox import Base, Student
 
@@ -12,5 +14,38 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    albert_einstein = Student(
+        name="Albert Einstein",
+        email="albert.einstein@zurich.edu",
+        grade=6,
+        # birthday=datetime.datetime.now()
+        birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+    alan_turing = Student(
+        name="Alan Turing",
+        email="alan.turing@sherborne.edu",
+        grade=11,
+        birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+    #session.add(albert_einstein)
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
     
-    import ipdb; ipdb.set_trace()
+    students = session.query(Student)
+    print([student for student in students])
+    # print(f"New student ID is {albert_einstein.id}.")
+    # print(f"New student ID is {alan_turing.id}.")
+
+
+    
+    # ipdb.set_trace()
+
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -10,12 +10,31 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'),
+        UniqueConstraint(
+            'email',
+            name='unique_email'),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12')
+    )
 
-    id = Column(Integer(), primary_key=True)
+    Index('index_name', 'name')
+
+    id = Column(Integer())
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///:memory:')
-    Base.metadata.create_all(engine)
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
